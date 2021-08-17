9398134046836c5455c1530392c58b14d5c03780
Sohini Pattanayak <sohinipattanayak.it@gmail.com>
2021-08-17 17:08:45 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express()
+const { ApolloClient, InMemoryCache, HttpLink, gql } = require('@apollo/client');
+const fetch = require('node-fetch');
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: process.env['GRAPHQL_ENDPOINT'], fetch, headers: {
+      'Authorization': `Bearer ${process.env['GITHUB_API_TOKEN']}`,
+    },
+  }),
+  cache
+});
+
+app.get('/', (req, res) =>
+  client
+    .query({
+      query: gql`{
+  search(query: "topic:alexa-skill-template sort:updated-desc", type: REPOSITORY, first: 5) {
+    repositoryCount
+    nodes {
+      ... on Repository {
+        nameWithOwner
+        description
+        updatedAt
+        createdAt
+        diskUsage
+      }
+    }
+  }
+}
+`})
+    .then(res => {
+      const data = JSON.stringify(res)
+      console.log(data)
+      })
+    .catch(error => console.error(error))
+);
+
+app.listen(8000, () => console.log(`Example app listening on port 8000!`))
